666d865266dfc559de0f3f3e814b1ad082bbf701
Ian Conery <ianconery@gmail.com>
2016-04-25 15:37:00 -0700

    cleanup for phantom.js, going server side yay

--- differentApproach.js
+++ differentApproach.js
@@ -1,3 +1,4 @@
 //I am going to try a different approach to this problem, and use a different library or libraries if this doesn't work I will have to rewrite the libraries that we are currently using.
 //First I am going to start with Mozillas pdf.js and see if I can get some good results with that.
+//I also want to look into phantom.js on the server side.
 
