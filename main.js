fee17fd81a2dedd1e14706aa234a12278b0b11d4
Ian Conery <ianconery@gmail.com>
2016-04-26 08:44:01 -0700

    messing around with phantom

--- main.js
+++ main.js
@@ -1,8 +1,25 @@
 var page = require('webpage').create();
-page.open('https://github.com/IanConery', function(status){
-  console.log(status);
-  if(status === 'success'){
-    page.render('github.png')
+var system = require('system');
+
+if(system.args.length === 1){
+  console.log('Usage: main.js <https://some URL>');
+  phantom.exit();
+}
+
+var t = Date.now();
+var address = system.args[1];
+page.open(address, function(status){
+  if(status !== 'success'){
+    console.log('Failed to load' + address);
+  }else{
+    var title = page.evaluate(function(){
+      return document.title;
+    });
+    t = Date.now() - t;
+    console.log('Loading ' + system.args[1]);
+    console.log('Loading time ' + t + ' msec');
+    console.log('Page title is ' + title);
   }
+  page.render('address.png');
   phantom.exit();
 });
